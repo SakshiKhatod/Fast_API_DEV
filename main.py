7ac2f590aa0fc78dde368b1961c5f5871f6326ef
Sakshi Khatod <sakshi.khatod1998@gmail.com>
2025-05-28 16:41:59 +0530

    filtering and sorting patients

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Path, HTTPException
+from fastapi import FastAPI, Path, HTTPException, Query
 import json
 
 app = FastAPI()
@@ -40,3 +40,26 @@ def view_patient_by_id(
     if patient_id in data:
         return data[patient_id]
     raise HTTPException(status_code=404, detail="Patient not found")
+
+
+@app.get("/sort")
+def sort_patients(
+    sort_by: str = Query(..., description="Sort on basis of hight,weight,bmi"),
+    order: str = Query("asc", description="Order of sorting: asc or desc"),
+):
+
+    valid_fields = ["height", "weight", "bmi"]
+    if sort_by not in valid_fields:
+        raise HTTPException(
+            status_code=400,
+            detail=f"Invalid sort field. Valid fields are: {', '.join(valid_fields)}",
+        )
+    if order not in ["asc", "desc"]:
+        raise HTTPException(status_code=400, detail="Order must be 'asc' or 'desc'")
+
+    sort_order = True if order == "desc" else False
+    data = load_data()
+    sorted_patients = sorted(
+        data.values(), key=lambda x: x.get(sort_by, 0), reverse=sort_order
+    )
+    return sorted_patients
