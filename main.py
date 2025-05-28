c68db408db20297a18c68dd16b9f7a8556596856
Sakshi Khatod <sakshi.khatod1998@gmail.com>
2025-05-28 16:05:28 +0530

    view patient by id

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ app = FastAPI()
 def hello():
     return {"message": "Patient Management System"}
 
+
 @app.get("/about")
 def about():
     return {"message": "This is a fully functional Patient Management System API."}
@@ -21,8 +22,17 @@ def greet(name: str):
 def load_data():
     with open("patients_data.json", "r") as file:
         return json.load(file)
-    
+
+
 @app.get("/view")
-def view_patients():
+def view_all():
     patients = load_data()
     return {"patients": patients}
+
+
+@app.get("/patient/{patient_id}")
+def view_patient_by_id(patient_id: str):
+    data = load_data()
+    if patient_id in data:
+        return data[patient_id]
+    return {"error": "Patient not found"}
